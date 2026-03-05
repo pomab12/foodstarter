49db88762750fc25192b06be108f4891a49ccf40
pomab <assefadagim@gmail.com>
2026-03-05 10:50:08 +0300

    project initialized first commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import netlify from '@netlify/vite-plugin-tanstack-start'
+import contentCollections from '@content-collections/vite'
+
+const config = defineConfig({
+  plugins: [
+    devtools(),
+    netlify(),
+    contentCollections(),
+    tsconfigPaths({ projects: ['./tsconfig.json'] }),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+  ],
+})
+
+export default config
